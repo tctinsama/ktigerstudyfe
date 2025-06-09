e4583ec743f57472dd8508960bb1b3d6ce3971e0
tctinsama <125227798+tctinsama@users.noreply.github.com>
2025-06-09 09:21:06 +0700

    refactor: organize admin and user folders

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
 import App from "./App.tsx";
-import { AppWrapper } from "./components/common/PageMeta.tsx";
+import { AppWrapper } from "./admin/components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
